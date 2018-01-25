9f497036c060b4b7aae0fe22ee9367f62d6d53c7
David D'Amato <ddamato@billtrust.local>
2018-01-25 12:57:20 -0700

    inital commit, minor performance bugs

---
+++ PasswordGuesser/PasswordGuesser/PasswordInfo.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+
+namespace PasswordGuesser
+{
+    public class PasswordInfo
+    {
+        private string password = null;
+        // more chars if we want ~!@#$%^&*()_-+=.<.>?|{}[]/*-+
+        private readonly char[] PossibleChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".ToCharArray();
+        private bool FoundInFile = false;
+        private int MaxLength = 10;
+        private const string ExceptionMsg = "Invalid password please try again.";
+        private const string FileNames = "10kMostCommonEnglishWords.txt,100kMostCommonEnglishWords.txt,100kMostCommonPasswords.txt";
+
+        public bool ContainedInFiles = false;
+        public int FileTries = 0;
+
+        public int BruteTries = 0;
+        public DateTime BruteForceEndTime = DateTime.MinValue;
+        
+        public PasswordInfo(string password)
+        {
+            this.password = password;
+        }
+
+        public void Validate()
+        {
+            if (password == null)
+            {
+                throw new Exception(ExceptionMsg);
+            }
+
+            if (password.Length > MaxLength)
+            {
+                Console.WriteLine($"Come on man that ones too hard. Use {MaxLength} or less chracters");
+                throw new Exception(ExceptionMsg);
+            }
+            if (this.password.Contains(" "))
+            {
+                Console.Write($"Pass phrase cracker is version 2.0, don't enter a password with spaces");
+                throw new Exception(ExceptionMsg);
+            }
+            var illegalChars = false;
+            foreach (var c in password)
+            {
+                if (!PossibleChars.Any(chars => chars == c))
+                {
+                    Console.WriteLine($"Invalid character detected: {c}");
+                    illegalChars = true;
+                }
+            }
+
+            if (illegalChars)
+            {
+                throw new Exception(ExceptionMsg);
+            }
+        }
+
+        /// <summary>
+        /// BruteForceCheckSameLength
+        /// </summary>
+        /// <param name="maxLength"></param>
+        /// <param name="position"></param>
+        /// <param name="startString"></param>
+        public void BruteForceCheckSameLength(int maxLength, int position = 0, string startString = "")
+        {
+            for (var count = 0; count < PossibleChars.Length; count++)
+            {
+                if (position < maxLength - 1)
+                {
+                    // recurse. TODO: theres got to be some way to bail on the first correct check, but break doesn't work. return an object maybe?
+                    BruteForceCheckSameLength(maxLength, position + 1, startString + PossibleChars[count]);
+                }
+                /* Commented out for performance, uncomment the line if you want to see the output, it drastically slows down performance.
+                 var check = startString + possibleChars[count];
+                 Console.WriteLine($"Checking {check}"); */
+                if (startString + PossibleChars[count] == this.password)
+                {
+                    Console.WriteLine($"Cracked! Password is: { this.password}");
+                    BruteForceEndTime = DateTime.Now;
+                }
+
+                // also has a performance implication
+                BruteTries++;
+            }
+        }
+
+        //TODO: check all password lengths for a more accurate time compare.
+            public void BruteForceCheckAll(int position = 0, string testString = "")
+        {
+            throw new NotImplementedException("TODO");
+        }
+
+        /// <summary>
+        /// loads about 200k common passwords and checks against the input 
+        /// </summary>
+        /// <param name="position"></param>
+        /// <param name="testString"></param>
+        public void GuessFromFiles(int position = 0, string testString = "")
+        {
+            // TODO add as content and use directly instead of loading from file.
+            foreach (var file in FileNames.Split(','))
+            {
+                var dir = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+                var path = Path.Combine($@"{dir}\Resources", file);
+                Console.WriteLine($"Checking contents of file: {path} ");
+                var content = File.ReadAllLines(path);
+                foreach (var line in content)
+                {
+                    if (password.ToLower().Equals(line.ToLower()))
+                    {
+                        Console.Write($"Your password was: {password} line was: {line}");
+                        ContainedInFiles = true;
+                        //TODO figure out case
+                        FoundInFile = true;
+                        break;
+                    }
+                    FileTries++;
+                }
+
+                if (FoundInFile)
+                {
+                    //TODO: refine files so they only contain unique content.
+                    Console.WriteLine($"Found match in resource file: {file}");
+                    break;
+                }
+
+            }
+
+        }
+
+    }
+
+
+}
---
+++ PasswordGuesser/PasswordGuesser/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Reflection.PortableExecutable;
+using System.Security.Cryptography;
+using System.Threading.Tasks.Dataflow;
+
+namespace PasswordGuesser
+{
+    class Program
+    {
+        private const string GnuMsg =
+            "This program is free software: you can redistribute it and/or modify\r\n    it under the terms of the GNU General Public License as published by\r\n    the Free Software Foundation, either version 3 of the License, or\r\n    (at your option) any later version.\r\n\r\n    This program is distributed in the hope that it will be useful,\r\n    but WITHOUT ANY WARRANTY; without even the implied warranty of\r\n    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\r\n    GNU General Public License for more details.\r\n\r\n    You should have received a copy of the GNU General Public License\r\n    along with this program.  If not, see <http://www.gnu.org/licenses/>.";
+        private const string Welcome = "Created by David D'Amato in 2017.";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"{GnuMsg} \r\n {Welcome}");
+            try
+            {
+                Console.WriteLine("This program explores passwords and how hard they are to guess using a brute force method. It is in no way efficent.");
+                while (true)
+                {
+                    Console.WriteLine("Input the password to check the complexity of: \r\n");
+                    var password = Console.ReadLine();
+                    var pwInfo = new PasswordInfo(password);
+                    pwInfo.Validate();
+
+                    var startTime = DateTime.Now;
+                    pwInfo.GuessFromFiles();
+                    var fileCheckTime = Math.Abs((startTime - DateTime.Now).TotalSeconds);
+                    Console.WriteLine($"Total Elapsed seconds from guessing from files {fileCheckTime}");
+
+                    startTime = DateTime.Now;
+                    Console.WriteLine("Starting brute force check... \r\nthis may take a long time, hours for 7-8 characters, days+ for 9+ depending on computer performance.");
+                    pwInfo.BruteForceCheckSameLength(password.Length);
+                    var bruteForceTime = Math.Abs((startTime - pwInfo.BruteForceEndTime).TotalSeconds);
+                    Console.WriteLine($"Total Elapsed seconds from brute force check: {bruteForceTime}");
+
+                    Console.WriteLine($"\r\n*****Summary***** \r\nTime to find from ~200k most common passwords: {fileCheckTime} seconds\r\nPassword In 200k most common passwords: {pwInfo.ContainedInFiles}");
+                    Console.WriteLine($"Time to check using brute force: {fileCheckTime} seconds \r\n ");
+                    Console.WriteLine($"Brute Force Tries: {pwInfo.BruteTries} \r\nCommon Password Tries: {pwInfo.FileTries}");
+                    Console.Write("Press C to contiue or any other key to exit...");
+
+                    var exit = Console.ReadKey().Key;
+                    if (exit != ConsoleKey.C)
+                    {
+                        Environment.Exit(0);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+    }
+
+
+}
