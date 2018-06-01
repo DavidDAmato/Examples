90dbe40ef596de14c081f88917f5c0e2d1f352b9
David D'Amato <ddamato@billtrust.local>
2018-05-31 19:03:43 -0600

    rev2

--- JSCalc/JSCalc/calculator.js
+++ JSCalc/JSCalc/calculator.js
@@ -1,29 +1,33 @@
-﻿var opperation = document.getElementById("searchTxt").value;
+﻿var opperation = document.getElementById("opperation").value;
 var a = document.getElementById("num1").value;
 var b = document.getElementById("num2").value;
 
-if (!(isNumber(a))) {
-    document.getElementById("error").innerHTML = "The first input must a valid integer, ex: 123.";
-}
-if (!(isNumber(b))) {
-    document.getElementById("error").innerHTML = "The second input must a valid integer, ex: 123.";
-}
+// do checks and math
+function Eval() {
+    // validate
+    if (!(isNumber(a))) {
+        document.getElementById("error").innerHTML = "The first input must a valid integer, ex: 123.";
+    }
+    if (!(isNumber(b))) {
+        document.getElementById("error").innerHTML = "The second input must a valid integer, ex: 123.";
+    }
 
+    // check opperation type
+    if (opperation === "+") {
+        document.getElementById("error").innerHTML = Add(a,b);
+    }
+    else if (opperation === "-") {
+        document.getElementById("error").innerHTML = Subtract(a, b);
+    }
+    else if (opperation === "*") {
+        document.getElementById("error").innerHTML = Multiply(a, b);
+    }
+    else if (opperation === "\\") {
+        document.getElementById("error").innerHTML = Divide(a, b);
+    }
+    else
+    {
+        document.getElementById("error").innerHTML = "Invalid opperation type, use +, -, *, or \\ ";
+    }
 
-if (input === "+") {
-    document.getElementById("error").innerHTML = Add(a,b);
 }
-else if(input === "-") {
-    document.getElementById("error").innerHTML = Subtract(a, b);
-}
-else if(input === "*") {
-    document.getElementById("error").innerHTML = Multiply(a, b);
-}
-else if(input === "\\") {
-    document.getElementById("error").innerHTML = Divide(a, b);
-}
-else
-{
-    document.getElementById("error").innerHTML = "Valid";
-}
-
