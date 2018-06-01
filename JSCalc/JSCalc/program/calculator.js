2020348a289dada4982ee1db46905f44570b3e3a
David D'Amato <ddamato@billtrust.local>
2018-06-01 16:47:37 -0600

    unit tests work with demo project

---
+++ JSCalc/JSCalc/program/calculator.js
@@ -0,0 +1,78 @@
+﻿// See: https://embed.plnkr.co/9hK5Lj/
+function Calculator() {
+}
+
+Calculator.prototype.add = function () {
+    var operands = Array.prototype.slice.call(arguments);
+    var sum = 0;
+    for (var i = 0; i < operands.length; i++) {
+        sum += operands[i];
+    }
+
+    return sum;
+}
+/*
+
+// return true if the number is either all numbers, 1234, or parsable as a int, "123455"
+function isNumber(n) {
+    return /^-?[\d.]+(?:e-?\d+)?$/.test(n);
+} 
+
+export class MyMath {
+    // add 2 numbers and return result
+    Add(a, b) {
+        return Number(a) + Number(b);
+    }
+    // subtract 2 numbers and return result
+    Subtract(a, b) {
+        return Number(a) - Number(b);
+    }
+    // multiply 2 numbers and return result
+    Multiply(a, b) {
+        return Number(a) * Number(b);
+    }
+    // divide 2 numbers and return result
+    Divide(a, b) {
+        return Number(a) / Number(b);
+    }
+}
+
+// do checks and math
+function RunCalculator() {
+    var a = document.getElementById("num1").value;
+    var b = document.getElementById("num2").value;
+    var opperation = document.getElementById("opperation").value;
+    var newMath = new MyMath();
+    var docInnerHtml = document.getElementById("answer");
+
+    // validate
+    if (!(isNumber(a))) {
+        docInnerHtml.innerHTML = "The first input must a valid integer, ex: 123.";
+    }
+    else if (!(isNumber(b))) {
+        docInnerHtml.innerHTML = "The second input must a valid integer, ex: 123.";
+    }
+    else
+    {
+        // check opperation type
+        if (opperation === "+") {
+            docInnerHtml.innerHTML = newMath.Add(a, b);
+        }
+        else if (opperation === "-") {
+            docInnerHtml.innerHTML = newMath.Subtract(a, b);
+        }
+        else if (opperation === "*") {
+            docInnerHtml.innerHTML = newMath.Multiply(a, b);;
+        }
+        else if (opperation === "\\") {
+            docInnerHtml.innerHTML = newMath.Divide(a, b);
+        }
+        else {
+            docInnerHtml.innerHTML = "Invalid opperation type, use +, -, *, or \\ ";
+        }
+
+    }
+    
+}
+
+*/
---
+++ JSCalc/JSCalc/test/calculator.tests.js
@@ -0,0 +1,34 @@
+﻿describe('Given the Calculator', function() {
+    var calc = new Calculator();
+    var sum;
+
+    it('Then it contains an add method', function() {
+        expect(calc.add).toBeDefined();
+    });
+
+    describe('When I add two numbers', function() {
+
+        it('Then the I get the correct answer', function() {
+            sum = calc.add(1, 2);
+            expect(sum).toBe(3);
+        });
+    });
+
+    describe('When I add three numbers', function() {
+
+        it('Then the I get the correct answer', function() {
+            sum = calc.add(1, 2, 7);
+            expect(sum).toBe(10);
+        });
+    });
+});
+
+/*
+
+describe("Add", function () {
+    it("Adds two integers", function () {
+        var result = calculator.Add(5, 10);
+        expect(result).toEqual(15);
+    });
+}).call(this);
+*/
